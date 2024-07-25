13ee6959e9bc086ec3a03d78e22d4c3c1f9f8468
zuldare <jaime.hernandezortiz@gmail.com>
2024-07-25 22:17:41 +0200

    first commit

---
+++ backapp/src/main/java/com/jh/backapp/BackappApplication.java
@@ -0,0 +1,15 @@
+package com.jh.backapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackappApplication.class, args);
+    }
+
+}
+
---
+++ frontapp/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <button @click="fetchMessage">Fetch Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+  methods: {
+    fetchMessage() {
+      console.log("Before fecth");
+      fetch("http://localhost:8080/hello")
+        .then((response) => response.text())
+        .then((data) => {
+          this.message = data;
+          console.log("Response text:", data);
+        })
+        .catch((error) => {
+          console.error("Error fetching the message:", error);
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
